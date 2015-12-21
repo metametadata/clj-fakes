055d2b92d9674f1d697200f46994585261778636
metametadata <yuri.govor@gmail.com>
2015-12-21 18:24:48 +0200

    bumped version to 0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.4.0
+
+Fixed:
+- Clojurescript is a hard dependency #1.
+
 ## 0.3.0
 
 - Public release.
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-fakes "0.4.0-SNAPSHOT"
+(defproject clj-fakes "0.4.0"
   :description "An isolation framework for Clojure/ClojureScript. It makes creating mocks and stubs for unit testing much easier."
   :url "https://github.com/metametadata/clj-fakes"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
