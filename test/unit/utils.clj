71ed5225ebb7974b2a4e70b137effe03388a3d52
metametadata <yuri.govor@gmail.com>
2016-07-27 18:38:47 +0300

    removed unused nodoc metadata from tests

--- test/unit/utils.clj
+++ test/unit/utils.clj
@@ -2,7 +2,7 @@
   (:require [clj-fakes.macro :refer [-cljs-env?]]
             [clojure.string :as string]))
 
-(defmacro ^:no-doc -deftest
+(defmacro -deftest
   "The same as deftest but name is defined using a string.
   Inspired by: https://gist.github.com/mybuddymichael/4425558"
   [name-string & body]
@@ -16,7 +16,7 @@
                         symbol)]
     `(~deftest ~name-symbol ~@body)))
 
-(defmacro ^:no-doc -is-exception-thrown
+(defmacro -is-exception-thrown
   "(is (thrown-with-msg? ...)) for specified exceptions in Clojure/ClojureScript."
   [clj-exc-class cljs-exc-class re expr]
   (let [is (if (-cljs-env? &env) 'cljs.test/is
@@ -25,12 +25,12 @@
                                         clj-exc-class)]
     `(~is (~'thrown-with-msg? ~exc-class ~re ~expr))))
 
-(defmacro ^:no-doc -is-error-thrown
+(defmacro -is-error-thrown
   "(is (thrown-with-msg? ...)) for general exceptions in Clojure/ClojureScript."
   [re expr]
   `(-is-exception-thrown Exception js/Error ~re ~expr))
 
-(defmacro ^:no-doc -is-assertion-error-thrown
+(defmacro -is-assertion-error-thrown
   "(is (thrown-with-msg? ...)) for assert exceptions in Clojure/ClojureScript."
   [re expr]
   `(-is-exception-thrown AssertionError js/Error ~re ~expr))
